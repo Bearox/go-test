052a4e632a52684c7796cf30749e3b40adc1ec7d
weiwen <weiwen@qunhemail.com>
2021-05-27 19:12:37 +0800

    test for &

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,15 @@
-package hello
-import "fmt"
-import "rsc.io/quote"
+package main
+import (
+	"fmt"
+	"time"
+)
 import "github.com/Bearox/go-test/greetings"
 
 func main() {
 	fmt.Println("hello world.")
-	fmt.Println(quote.Go())
     fmt.Println(greetings.Hello("weiwen"))
+	fmt.Println(5 & 1)
+	fmt.Println(10 & 1)
+	fmt.Println(time.Now().Second() & 1)
+	fmt.Println(time.Now().Add( - time.Minute * 10))
 }
